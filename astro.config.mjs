f151937acc89e076f11b926ac2cce95c1f07b2b7
Jr-Yung Tsai <wemee7012@gmail.com>
2026-01-10 18:44:48 +0800

    feat: add sitemap generation and robots.txt

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,9 @@
 import { defineConfig } from 'astro/config';
 
+import sitemap from '@astrojs/sitemap';
+
 export default defineConfig({
   site: 'https://wemee.github.io',
   output: 'static',
+  integrations: [sitemap()],
 });
